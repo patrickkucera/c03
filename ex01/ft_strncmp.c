342ad69cb307baf9dba747ac267b8a0ae67dfcb0
pakucera <pakucera@student.42lausanne.ch>
2021-09-18 22:53:00 +0200

    new commit

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -1,36 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pakucera <pakucera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/18 16:12:20 by pakucera          #+#    #+#             */
-/*   Updated: 2021/09/18 17:34:27 by pakucera         ###   ########.fr       */
+/*   Created: 2021/09/18 17:28:52 by pakucera          #+#    #+#             */
+/*   Updated: 2021/09/18 22:52:48 by pakucera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <string.h>
 
-int	ft_strcmp(char *s1, char *s2)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+	if (n == 0)
+	{
+		return (0);
+	}
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
 	{
 		i++;
 	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
-int	main(void)
+/*int	main(void)
 {
-	char	s1[256] = "asdfgg";
-	char	s2[256] = "asdfg";
-	ft_strcmp(s1, s2);
-	printf("original Result: %d\n", strcmp(s1, s2));
-	printf("Result: %d\n", ft_strcmp(s1, s2));
+	char	s1[256] 	= "ababaaaaaaaaab";
+	char	s2[256] 	= "aabbaaaaaaaab";
+	printf("Result: %d\n", ft_strncmp(s1, s2, 6));
 	return (0);
-}
+}*/
