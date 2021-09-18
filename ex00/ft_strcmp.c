571e4782adc8c681a83d0011651291b352bcee8b
pakucera <pakucera@student.42lausanne.ch>
2021-09-18 17:26:13 +0200

    commit c03ex00

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -6,25 +6,31 @@
 /*   By: pakucera <pakucera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/18 16:12:20 by pakucera          #+#    #+#             */
-/*   Updated: 2021/09/18 16:28:33 by pakucera         ###   ########.fr       */
+/*   Updated: 2021/09/18 17:25:58 by pakucera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
 
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] == s2[i])
-		{
-			i++;
-			if (s1[i] < s2[i] || s1[i] > s2[i])
-			{
-				return (s1[i] - s2[i]);
-			}
-			return (0);
-		}	
+		i++;
 	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
+
+/*int	main(void)
+{
+	char	s1[256] = "asdfgg";
+	char	s2[256] = "asdfg";
+	ft_strcmp(s1, s2);
+	printf("original Result: %d\n", strcmp(s1, s2));
+	printf("Result: %d\n", ft_strcmp(s1, s2));
+	return (0);
+}*/
