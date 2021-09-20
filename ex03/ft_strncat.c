2aeff775744e5fe12d1f32b9f7b91b057bd47a0c
pakucera <pakucera@student.42lausanne.ch>
2021-09-20 22:40:40 +0200

    fgs

--- ex03/ft_strncat.c
+++ ex03/ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: pakucera <pakucera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 20:55:55 by pakucera          #+#    #+#             */
-/*   Updated: 2021/09/20 22:39:46 by pakucera         ###   ########.fr       */
+/*   Updated: 2021/09/20 22:40:26 by pakucera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,10 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 	{
 		*dest++ = *src++;
 		i++;
-		
 	}
 	*dest = '\0';
 	return (a);
+}
 
 /* int	main(void)
 {
