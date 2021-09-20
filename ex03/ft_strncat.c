41f1d221bf4e1ec45f75b7163ac5a9b066c29e7d
pakucera <pakucera@student.42lausanne.ch>
2021-09-20 21:44:48 +0200

    Ex03 : wip

---
+++ ex03/ft_strncat.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pakucera <pakucera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/20 20:55:55 by pakucera          #+#    #+#             */
+/*   Updated: 2021/09/20 21:44:35 by pakucera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	char			*a;
+	unsigned int	i;
+
+	a = dest;
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	i = 1;
+	while (*src != '\0' && i <= nb)
+	{
+		*dest++ = *src++;
+		i++;
+	}
+	return (a);
+}
+
+int	main(void)
+{
+	char	test[256] = "\0zxcvzxcvzxcvxzcvzxcvzxcv";
+	printf("%s\n", ft_strncat(test, "asdf", 16));
+	printf("%s\n", ft_strncat(test, "", 16));
+	printf("%s\n", ft_strncat(test, "qwerty", 0));
+	printf("%s\n", ft_strncat(test, "asdf", 3));
+}
