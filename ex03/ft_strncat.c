a8433700e1c3ce1918500589941a422f87600e97
pakucera <pakucera@student.42lausanne.ch>
2021-09-20 21:56:34 +0200

    nc

--- ex03/ft_strncat.c
+++ ex03/ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: pakucera <pakucera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 20:55:55 by pakucera          #+#    #+#             */
-/*   Updated: 2021/09/20 21:44:35 by pakucera         ###   ########.fr       */
+/*   Updated: 2021/09/20 21:56:13 by pakucera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-	char			*a;
+/* 	char			*a;
 	unsigned int	i;
 
 	a = dest;
@@ -29,14 +29,38 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 		*dest++ = *src++;
 		i++;
 	}
-	return (a);
+	return (a); */
+
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i])
+	{
+		i++;
+	}
+	printf("%d", i);
+	while (src[j] && j <= nb)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+/* 	while (*src != '\0' && i <= nb)
+	{
+		*dest++ = *src++;
+		i++;
+	} */
 }
 
-int	main(void)
+/*int	main(void)
 {
-	char	test[256] = "\0zxcvzxcvzxcvxzcvzxcvzxcv";
-	printf("%s\n", ft_strncat(test, "asdf", 16));
-	printf("%s\n", ft_strncat(test, "", 16));
-	printf("%s\n", ft_strncat(test, "qwerty", 0));
-	printf("%s\n", ft_strncat(test, "asdf", 3));
-}
+	char	dest[256] = "\0nabcd";
+	printf("%s\n", ft_strncat(dest, "efghij", 6));
+	printf("%s\n", ft_strncat(dest, "", 16));
+	printf("%s\n", ft_strncat(dest, "qwerty", 0));
+	printf("%s\n", ft_strncat(dest, "asdf", 3));
+}*/
