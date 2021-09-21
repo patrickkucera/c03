7b20015a4b506a48b20fa305b980bd04403a1960
pakucera <pakucera@student.42lausanne.ch>
2021-09-21 02:21:50 +0200

    commit

--- ex02/ft_strcat2.c
+++ ex02/ft_strcat2.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   asad.c                                             :+:      :+:    :+:   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pakucera <pakucera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/21 02:11:58 by pakucera          #+#    #+#             */
-/*   Updated: 2021/09/21 02:20:14 by pakucera         ###   ########.fr       */
+/*   Created: 2021/09/18 22:55:56 by pakucera          #+#    #+#             */
+/*   Updated: 2021/09/21 01:59:26 by pakucera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-char	*ft_strcat(char *dest,  char *src)
+char	*ft_strcat(char *dest, char *src)
 {
 	int	i;
 	int	j;
@@ -29,8 +29,26 @@ char	*ft_strcat(char *dest,  char *src)
 		dest[i + j] = src[j];
 		j++;
 	}
+	dest[i + j] = '\0';
 	return (dest);
 }
+
+/*int main(void) {
+  char s1[30] = "hello";  
+  char s2[] = ", world\n";
+ 
+  strcat(s1, s2);
+  printf("%s", s1);
+  return 0;
+}*/
+
+/*int main(void)
+{
+	char test[256] = "";
+	printf("%s\n", ft_strcat(test, "asdf"));
+	printf("%s\n", ft_strcat(test, ""));
+	printf("%s\n", ft_strcat(test, "zxcv"));
+}*/
 
 int	main(void)
 {
@@ -69,3 +87,7 @@ int	main(void)
 	exp = strcat(test3, btest3);
 	printf("%s\n%s -> %d\n", res, exp, res == exp);	
 }
+
+/* 
+==== check -e ====
+$expected = "asdf\nasdf\nasdfzxcv\n";*/
